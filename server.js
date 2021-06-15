c888c7a78d88cbf32106358a6ec80dde2475a7e1
pyae-phyo-thant <pyaephyothant1998@gmail.com>
2021-06-15 11:26:54 +0630

    prepare for deploy

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use("/api/profile", require("./routes/api/profile"));
 //serve static assets in producation
 if (process.env.NODE_ENV === "production") {
   //Set static doler
-  app.use(express.static("../frontend/build"));
+  app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
